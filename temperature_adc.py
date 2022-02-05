b5629759a81c379c69e992302c2973c3355c2478
javad pournosrat <pournosrat.javad@gmail.com>
2022-02-06 00:22:46 +0330

    concatinate tempurature to 5 character and create cutome character for tempurature

--- temperature_adc.py
+++ temperature_adc.py
@@ -17,7 +17,22 @@ conversion_factor = 3.3/(65535)
 while True:
     reading = sesore_temp.read_u16() * conversion_factor
     tempurature = 27 - (reading - 0.706)/0.001721
-    print(tempurature)
-    lcd.putstr('Tempurature:\n' + str(tempurature))
+    #print(tempurature)
+    lcd.putstr('Tempurature:\n' + str(tempurature)[0:5])
+    lcd.custom_char(0, bytearray([
+      0x07,
+      0x05,
+      0x07,
+      0x00,
+      0x00,
+      0x00,
+      0x00,
+      0x00
+      ]))
+    
+    lcd.move_to(5, 1)
+    lcd.putchar(chr(0))
+    lcd.move_to(6, 1)
+    lcd.putstr('C')
     utime.sleep(2)
     lcd.clear()
